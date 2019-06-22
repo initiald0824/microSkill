d12cceb15c17ca299288d4232ca2007f93b9b97b
initiald0824 <initiald0824@163.com>
2019-06-22 19:40:51 +0800

    first commit

---
+++ src/main/java/com/study/microskill/MicroskillApplication.java
@@ -0,0 +1,13 @@
+package com.study.microskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroskillApplication.class, args);
+    }
+
+}
